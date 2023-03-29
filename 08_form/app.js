83b369f1fbeba0ce23f6f8eb64c7699eba566ea9
KIMSEUNGYOON <tmddbs7854@naver.com>
2023-03-29 20:30:34 +0900

    study: get 정보받기 문제

--- 08_form/app.js
+++ 08_form/app.js
@@ -11,29 +11,33 @@ app.use(express.json()); // json 형태로 데이터를 주고 받음
 // 브라우저에서 어떤 url로 접속했을 때 어떤 페이지를 보여줄 것인가!!!
 
 // localhost:PORT/ 접속했을 때, index.ejs 를 보여주겠다
+
 app.get("/", function (req, res) {
+  res.render("index", {
+    title: "실습풀이",
+  });
+});
+app.get("/practice1", function (req, res) {
   // views/index.ejs 파일을 찾아서 응답
-  const myTitle = "폼 실습을 합시다~~~";
-  res.render("index", { title: myTitle });
+  res.render("practice1");
 });
 
-app.get("/getForm", function (req, res) {
+app.get("/getForm1", function (req, res) {
   // console.log(req); // { xx: {},  yy: {}, ... query: { id: 'sean', pw: '1234' }}
   console.log(req.query); // { id: 'sean', pw: '1234' }
-  res.render("result", {
-    title: "GET 요청 폼 확인하기",
+  res.render("result1", {
     userInfo: req.query,
   });
 });
 
-app.post("/postForm", function (req, res) {
-  console.log(req.body); // { id: 'apple', pw: '1234' }
-  //  res.send("post 요청 성공!!!");
-  res.render("result", {
-    title: "POST 요청 폼 확인하기",
-    userInfo: req.body,
-  });
-});
+// app.post("/postForm", function (req, res) {
+//   console.log(req.body); // { id: 'apple', pw: '1234' }
+//   //  res.send("post 요청 성공!!!");
+//   res.render("result", {
+//     title: "POST 요청 폼 확인하기",
+//     userInfo: req.body,
+//   });
+// });
 
 app.listen(PORT, function () {
   console.log("웹 서버 실행!!");
